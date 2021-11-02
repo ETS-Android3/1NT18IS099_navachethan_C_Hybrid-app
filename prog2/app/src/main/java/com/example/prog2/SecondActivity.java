af87671f2a36b2292b9b04a9bb673a577759d6c2
Admin <mnavachethan12@yahoo.com>
2021-11-02 12:48:20 +0530

    second prog

---
+++ prog2/app/src/main/java/com/example/prog2/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.prog2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+   TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        txt=findViewById(R.id.textView2);
+        Bundle b = getIntent().getExtras();
+        String str1 = b.getString("name");
+        txt.setText(str1);
+
+    }
+}
